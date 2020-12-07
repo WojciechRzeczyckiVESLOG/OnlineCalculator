bfa85f173142e94af839b887cd0e0cf290855917
Mateusz Sierszulski <mateusz.sierszulski@gmail.com>
2020-12-07 19:44:27 +0100

    Add keyboard events

--- Client/client/CalcWindow.xaml.cs
+++ Client/client/CalcWindow.xaml.cs
@@ -37,6 +37,86 @@ namespace GUI
             this.client = client;
         }
 
+        private void keyboardHandler(object sender, KeyEventArgs e)
+        {
+            if (WPFequation.Text == "Enter your favourite equation . . .") WPFequation.Text = "";
+
+            if (e.Key == Key.NumPad0 || e.Key == Key.D0)
+            {
+                equationText += "0";
+                WPFequation.Text += "0";
+            }
+            if (e.Key == Key.NumPad1 || e.Key == Key.D1)
+            {
+                equationText += "1";
+                WPFequation.Text += "1";
+            }
+            if (e.Key == Key.NumPad2 || e.Key == Key.D2)
+            {
+                equationText += "2";
+                WPFequation.Text += "2";
+            }
+            if (e.Key == Key.NumPad3 || e.Key == Key.D3)
+            {
+                equationText += "3";
+                WPFequation.Text += "3";
+            }
+            if (e.Key == Key.NumPad4 || e.Key == Key.D4)
+            {
+                equationText += "4";
+                WPFequation.Text += "4";
+            }
+            if (e.Key == Key.NumPad5 || e.Key == Key.D5)
+            {
+                equationText += "5";
+                WPFequation.Text += "5";
+            }
+            if (e.Key == Key.NumPad6 || e.Key == Key.D6)
+            {
+                equationText += "6";
+                WPFequation.Text += "6";
+            }
+            if (e.Key == Key.NumPad7 || e.Key == Key.D7)
+            {
+                equationText += "7";
+                WPFequation.Text += "7";
+            }
+            if (e.Key == Key.NumPad8 || e.Key == Key.D8)
+            {
+                equationText += "8";
+                WPFequation.Text += "8";
+            }
+            if (e.Key == Key.NumPad9 || e.Key == Key.D9)
+            {
+                equationText += "9";
+                WPFequation.Text += "9";
+            }
+            if (e.Key == Key.Add)
+            {
+                equationText += "+";
+                WPFequation.Text += "+";
+            }
+            if (e.Key == Key.Subtract)
+            {
+                equationText += "-";
+                WPFequation.Text += "-";
+            }
+            if (e.Key == Key.Multiply)
+            {
+                equationText += "*";
+                WPFequation.Text += "*";
+            }
+            if (e.Key == Key.Divide)
+            {
+                equationText += "/";
+                WPFequation.Text += "/";
+            }
+            if (e.Key == Key.Enter)
+            {
+                buttonEqualizeClicked(sender, e);
+            }
+        }
+
         private void buttonClicked(object sender, RoutedEventArgs e)
         {
             if (equationText == "") WPFequation.Text = "";
@@ -52,7 +132,6 @@ namespace GUI
                 client.receivedMessage = ""; 
                 client.SendMessage((equationText));
                 client.TryReceive();
-
             }
             catch (SocketException)
             {
